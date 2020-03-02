00d66bb8e50fbaf6880533fc28a14a43619ee07f
baronTommy <16768208+baronTommy@users.noreply.github.com>
2020-03-02 23:28:10 +0900

    _

--- src/components/ecosystems/Some/CSSGrid.tsx
+++ src/components/ecosystems/Some/CSSGrid.tsx
@@ -1,4 +1,31 @@
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 import styled from "styled-components";
 
-export const CSSGrid: FC = () => <>p7</>;
+const Container = styled.div`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: 1fr 2fr;
+  grid-template-rows:
+    200px
+    100px
+    50px;
+`;
+
+const Item = styled.div`
+  background: #0bd;
+  color: #fff;
+`;
+
+export const CSSGrid: FC = () => (
+  <>
+    <h1>CssGridなのだよ</h1>
+    <Container>
+      <Item>Aaa</Item>
+      <Item>Bbb</Item>
+      <Item>Ccc</Item>
+      <Item>Ddd</Item>
+      <Item>Eee</Item>
+      <Item>Fff</Item>
+    </Container>
+  </>
+);
